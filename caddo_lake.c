82a1b001084b06131b2204cf6893d53fe56836e1
Francys Samuel Oliveira Pereira dos Santos <samueloliveirasantos003@gmail.com>
2026-09-17 05:19:18 -0300

    feat: time travel function, update anna's details and add executable file to gitignore

--- caddo_lake.c
+++ caddo_lake.c
@@ -13,17 +13,42 @@ struct Person{
     bool isTimeTraveler;
 };
 
-// time travel fuction (loop)
+void timeTravel(struct Person *p){
+    if(p->isTimeTraveler){
+        p->yoa = 1952;
+        p->age = 55;
+        strcpy(p->name, "Anna Lang");
+        strcpy(p->partner, "Benjamim Lang");
+        printf("\n%s has traveled to the year %d.\n", p->name, p->yoa);
+    } 
+    else {
+        printf("\n%s is not a time traveler.\n", p->name);
+    }
+}
 
 // quantum entanglement function (results of familiar lineage from the time travel)
 
 int main(){
+    int currentYear = 2024;
+
+    // Anna
 
     struct Person person1;
 
-    strcpy(person1.name, "Anna Lang");
+    strcpy(person1.name, "Anna Bennet");
+    person1.age = 8;
+    person1.isTimeTraveler = true;
 
-    printf("Name: %s\n", person1.name);
+    printf ("\n%s's current year is %d.\n", person1.name, currentYear);
+
+    printf ("\nBefore time travel:\n -name: %s\n -age: %d\n", person1.name, person1.age);
+
+    timeTravel(&person1);
+
+    printf ("\nAfter time travel:\n -name: %s\n -age: %d\n -partner: %s\n", person1.name, person1.age, person1.partner);
+
+    // Paris ...
+
 
     return 0;
     
