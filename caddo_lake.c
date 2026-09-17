7f0a012eb906089a9c0041b7652023aaffa235f6
Francys Samuel Oliveira Pereira dos Santos <samueloliveirasantos003@gmail.com>
2026-09-17 04:11:58 -0300

    feat: define person struct with attributes

--- caddo_lake.c
+++ caddo_lake.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define MAX 100
+
+struct Person{
+    char name[MAX];
+    int age;
+    int yob; // year of birth
+    int yod; // year of death
+    int yoa; // year of arrival
+    bool isTimeTraveler;
+};
 
 int main(){
     
