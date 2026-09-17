9b445b3507afbc0d80e2cd2087822595b84031e9
Francys Samuel Oliveira Pereira dos Santos <samueloliveirasantos003@gmail.com>
2026-09-17 04:17:44 -0300

    feat: add partner attribute and future function

--- caddo_lake.c
+++ caddo_lake.c
@@ -4,12 +4,17 @@
 
 struct Person{
     char name[MAX];
+    char partner[MAX];
     int age;
     int yob; // year of birth
     int yod; // year of death
     int yoa; // year of arrival
     bool isTimeTraveler;
 };
+
+// time travel fuction (loop)
+
+// quantum entanglement  function (results of familiar lineage from the time travel)
 
 int main(){
     
